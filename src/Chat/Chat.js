c3b62380012f5c6f1e12a6c69d4d93184bba4fce
Jiajie <jiajie.ma@thoughtworks.com>
2020-10-30 10:01:13 +0800

    [jiajie]finish all requirement

--- src/Chat/Chat.js
+++ src/Chat/Chat.js
@@ -5,6 +5,7 @@ import ChatBox from './ChatBox/ChatBox';
 import ChatInput from './ChatInput/ChatInput';
 import shopData from '../data/shop.json';
 import answersData from '../data/answers.json';
+import { ROLE } from '../constants';
 
 class Chat extends Component {
   constructor(props, context) {
@@ -27,13 +28,30 @@ class Chat extends Component {
     }, 1000);
   }
 
+  putMessage = (msg) => {
+    this.setState((state) => ({
+      messages: state.messages.concat(this.makeMsg(msg)),
+    }));
+  };
+
+  makeMsg = (msg) => {
+    const customerMsg = {
+      text: msg,
+      role: ROLE.CUSTOMER,
+    };
+    const answers = answersData.filter((answer) =>
+      answer.tags.find((tag) => customerMsg.text.includes(tag))
+    );
+    return [customerMsg].concat(answers);
+  };
+
   render() {
     const { shop, messages } = this.state;
     return (
       <main className="Chat">
         <ChatHeader shop={shop} />
         <ChatBox messages={messages} />
-        <ChatInput />
+        <ChatInput callback={this.putMessage} />
       </main>
     );
   }
--- src/Chat/ChatInput/ChatInput.js
+++ src/Chat/ChatInput/ChatInput.js
@@ -2,11 +2,30 @@ import React, { Component } from 'react';
 import './ChatInput.scss';
 
 class ChatInput extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      msg: '',
+    };
+  }
+
+  udateMsg = (event) => {
+    this.setState({
+      msg: event.target.value,
+    });
+  };
+
+  handlerClick = () => {
+    this.props.callback(this.state.msg);
+  };
+
   render() {
     return (
       <footer className="ChatInput">
-        <input type="text" />
-        <button type="button">Send</button>
+        <input type="text" onChange={this.udateMsg} />
+        <button type="button" onClick={this.handlerClick}>
+          Send
+        </button>
       </footer>
     );
   }
